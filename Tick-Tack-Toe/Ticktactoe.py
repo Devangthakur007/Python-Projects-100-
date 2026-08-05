b7da512428a3c2055df5176caed2cf4be97a76f0
Devang Thakur <theimortalempror@gmail.com>
2026-08-05 23:25:07 +0530

    Add files via upload

---
+++ Tick-Tack-Toe/Ticktactoe.py
@@ -0,0 +1,15 @@
+import sys
+
+def print_board(board: list[list[str]]):
+    """Print the current 3x3 game board nicely."""
+    print("\n")
+    print(f" {board[0][0]} | {board[0][1]} | {board[0][2]} ")
+    print("---+---+---")
+    print(f" {board[1][0]} | {board[1][1]} | {board[1][2]} ")
+    print("---+---+---")
+    print(f" {board[2][0]} | {board[2][1]} | {board[2][2]} ")
+    print("\n")
+
+def check_win(board: list[li])
+    
+    
